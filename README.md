87af61981ef4dda48fe5a44f3aef538551cafec9
sylilancanlions <15ymluo@stu.edu.cn>
2020-05-14 15:45:38 +0800

    exstract github infomation

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ spring
 spring mvc
 mybatis
 mysql/h2
+
+
+
+## 资料
+
+[bootstrap导航条](https://v3.bootcss.com/components/#navbar)
+[github oauth doc](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
---
+++ src/main/java/com/lion/community/controller/AuthorizeController.java
@@ -0,0 +1,43 @@
+package com.lion.community.controller;
+
+import com.lion.community.Provider.GithubProvider;
+import com.lion.community.dto.AccesstokenDTO;
+import com.lion.community.dto.GithubUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code")String code,
+                           @RequestParam(name="state")String state){
+        AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
+        accesstokenDTO.setClient_id(clientId);
+        accesstokenDTO.setClient_secret(clientSecret);
+        accesstokenDTO.setCode(code);
+        accesstokenDTO.setState(state);
+        accesstokenDTO.setRedirect_uri(redirectUri);
+        String accessToken =  githubProvider.getAccessToken(accesstokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user);
+        System.out.println(user.getName());
+        System.out.println(user.getBio());
+
+        return "index";
+    }
+}
