2b43099bd83892a2eb40887cb6103720b80a5dd2
sylilancanlions <15ymluo@stu.edu.cn>
2020-05-15 11:36:35 +0800

    finish how to connect to the database and insert the user data

--- src/main/java/com/lion/community/controller/AuthorizeController.java
+++ src/main/java/com/lion/community/controller/AuthorizeController.java
@@ -3,12 +3,17 @@ package com.lion.community.controller;
 import com.lion.community.Provider.GithubProvider;
 import com.lion.community.dto.AccesstokenDTO;
 import com.lion.community.dto.GithubUser;
+import com.lion.community.mapper.UserMapper;
+import com.lion.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -22,10 +27,13 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
         accesstokenDTO.setClient_id(clientId);
         accesstokenDTO.setClient_secret(clientSecret);
@@ -33,11 +41,26 @@ public class AuthorizeController {
         accesstokenDTO.setState(state);
         accesstokenDTO.setRedirect_uri(redirectUri);
         String accessToken =  githubProvider.getAccessToken(accesstokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user);
-        System.out.println(user.getName());
-        System.out.println(user.getBio());
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser!=null){
+            //登陆成功，写cookie和session
+            //这个函数会自动写cookie
 
-        return "index";
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("githubUser",githubUser);
+            return "redirect:/";
+        }
+        else{
+            //登陆失败，重新登陆
+
+            return "redirect:/";
+        }
+
     }
 }
---
+++ src/main/java/com/lion/community/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.lion.community.mapper;
+
+
+import com.lion.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("INSERT INTO user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
